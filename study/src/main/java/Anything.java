49eedd5b11caf11f657cc0f81e2ce9e5825c41fd
zhull <zhull_julie@163.com>
2018-03-15 11:47:03 +0800

    更新代码

--- study/src/main/java/Anything.java
+++ study/src/main/java/Anything.java
@@ -1,5 +1,4 @@
-import java.util.Enumeration;
-import java.util.Vector;
+import java.math.BigDecimal;
 
 /**
  * @author zhull
@@ -8,35 +7,18 @@ import java.util.Vector;
  */
 public class Anything {
 
-    public static void main(String[] args) throws Exception{
-        Vector v = new Vector();
-        for(int i = 0; i < 10; i++ )
-            v.addElement(new Int(i));
-        System.out.println("v: " + v);
-        Vector v2 = (Vector)v.clone();
-        // Increment all v2's elements:
-        for(Enumeration e = v2.elements();
-            e.hasMoreElements(); )
-            ((Int)e.nextElement()).increment();
-        // See if it changed v's elements:
-        System.out.println("v: " + v);
+    private static int _1MB = 1024*1024;
 
-        Int i = new Int(1);
-        Int i1 = (Int)i.clone();
 
-    }
-}
-
-class Int implements Cloneable {
-    private int i;
-    public Int(int ii) { i = ii; }
-    public void increment() { i++; }
-    public String toString() {
-        return Integer.toString(i);
+    public static void main(String[] args) {
+        System.out.println(getNormalAmount(20L, 3));
+        System.out.println(getNormalAmount(10L, 3));
+        System.out.println(getNormalAmount(13L, 2));
     }
 
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    static Long getNormalAmount(Long a, int b) {
+        BigDecimal normalAmount = new BigDecimal(a).divide(new BigDecimal(b), BigDecimal.ROUND_HALF_UP);
+        System.out.println("! " + normalAmount);
+        return normalAmount.longValue();
     }
 }
